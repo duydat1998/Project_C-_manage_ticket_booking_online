9aaa4468c4fa52040ac250408559eb7e8a402923
duydat1998 <32428939+duydat1998@users.noreply.github.com>
2018-06-18 13:14:00 +0700

    Add MovieDAO

---
+++ Project_TouchCinema/MovieLibrary/MovieDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MovieLibrary
+{
+    public class MovieDAO
+    {
+        private string strConnection;
+
+        public MovieDAO()
+        {
+            strConnection = ConfigurationManager.ConnectionStrings["TouchCinemaDB"].ConnectionString;
+
+        }
+
+        public String GetMovieTitle(string movieID)
+        {
+            String movieName = "";
+            SqlConnection conn = new SqlConnection(strConnection);
+            if (conn != null)
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                try
+                {
+                    string sql = "Select movieTitle from Movie where movieID=@id";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@id", movieID);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        
+                        movieName = reader["movieTitle"].ToString();
+                    }
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            return movieName;
+        }
+    }
+}
